7a1629b29a6e3fbc75b559c4209cf91fff4cc165
Abhinav <abhinavchavan0782@gmail.com>
2025-02-12 02:31:38 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import {PORT, mongoDBURL} from './config.js'
+import mongoose from 'mongoose'
+import userRoutes from './routes/userRoutes.js'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/', (req,res) => {
+    return res.status(234).send('<h1>HELLO WORLD</h1>')
+})
+
+app.use('/books',userRoutes)
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App connected to database.")
+        app.listen(PORT, () => {
+            console.log(`App is running on http://localhost:${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
